9e40a52879d8f195f8695f2e480fdee537a7d771
BlakeTrombone <blaketeastman@gmail.com>
2019-01-15 15:05:44 -0500

    this is supposed to work but it doesn't and ill fix it later

--- npc.java
+++ npc.java
@@ -6,8 +6,10 @@ public class npc
   public static piece[] turn(piece[] pieces)
   {
     piece[] output;
+    for (int x =0; x < pieces.length; x++)
+      pieces[x]=pieces[x].clone();
     for(int i =0; i < 32; i++)
-      moves[i]=new move(pieces, i/4 ,(i%4)+1);
+      moves[i]=new move(pieces.clone(), i/4 ,(i%4)+1);
     
     int curbest=0;
       for (int o=0; o < 5; o++)
