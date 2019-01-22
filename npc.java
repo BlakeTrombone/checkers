78fc1c8415b87ac7dd219b691509f493b3c2cd82
BlakeTrombone <blaketeastman@gmail.com>
2019-01-22 14:54:45 -0500

    I'm gonna do something different. We're gonna use sockets instead of a robot

--- npc.java
+++ npc.java
@@ -5,23 +5,7 @@ public class npc
   private static move[] moves = new move[32];
   public static piece[] turn(piece[] pieces)
   {
-    piece[] output;
-    for (int x =0; x < pieces.length; x++)
-      pieces[x]=pieces[x].clone();
-    for(int i =0; i < 32; i++)
-      moves[i]=new move(pieces.clone(), i/4 ,(i%4)+1);
-    
-    int curbest=0;
-      for (int o=0; o < 5; o++)
-        if(moves[o].total > moves[curbest].total)
-          curbest=o;
-      ArrayList<move> asdf = new ArrayList<move>();
-      for(move m : moves)
-        if(m.total>=moves[curbest].total)
-          asdf.add(m);
-      output=asdf.get((int)(Math.random()*asdf.size())).output;
-    
-    
+        
     
     return pieces;
   }
