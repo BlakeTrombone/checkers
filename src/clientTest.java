f611ec5cafeef030b46684a029848c804d5bc79b
Blake Eastman <blaketeastman@protonmail.com>
2019-04-03 16:55:53 -0400

    made code consistent with names

--- src/clientTest.java
+++ src/clientTest.java
@@ -9,37 +9,17 @@ public class clientTest {
     private static String serverIP;
     public static void main(String[] args)
     {
-
-        if(false)
-        {
-            try{
-                ds = new DatagramSocket(7059);
-                String ready="ready";
-                byte[] test = new byte[1024];
-                DatagramPacket dp = new DatagramPacket(test,test.length);
-                ds.receive(dp);
-                String info = new String(dp.getData());
-                ip=dp.getAddress();
-                String responce="ydaer";
-                DatagramPacket dps = new DatagramPacket(responce.getBytes(), responce.getBytes().length, ip, dp.getPort());
-                handshook=true;
-            } catch (Exception e){System.out.println("ERROR");}
-        }
-
-        if (!false)
-        {
-            try{
-                ds = new DatagramSocket();
-                ip = InetAddress.getByName("127.0.0.1");
-                String check="ready";
-                DatagramPacket dp = new DatagramPacket(check.getBytes(), check.getBytes().length,ip,7059);
-                ds.send(dp);
-                byte[] test = new byte[1024];
-                DatagramPacket dpr = new DatagramPacket(test, test.length);
-                ds.receive(dpr);
-                handshook=true;
-                System.out.println("client handshook");
-            } catch (Exception e){}
-        }
+        try{
+            ds = new DatagramSocket();
+            ip = InetAddress.getByName("127.0.0.1");
+            String check="ready";
+            DatagramPacket dp = new DatagramPacket(check.getBytes(), check.getBytes().length,ip,7059);
+            ds.send(dp);
+            byte[] test = new byte[1024];
+            DatagramPacket dpr = new DatagramPacket(test, test.length);
+            ds.receive(dpr);
+            handshook=true;
+            System.out.println("client handshook");
+        } catch (Exception e){}
     }
 }
--- src/serverTest.java
+++ src/serverTest.java
@@ -9,37 +9,18 @@ public class serverTest {
     private static String serverIP;
     public static void main(String[] args)
     {
-
-        if(true)
-        {
-            try{
-                ds = new DatagramSocket(7059);
-                byte[] test = new byte[1024];
-                DatagramPacket dp = new DatagramPacket(test,test.length);
-                ds.receive(dp);
-                String info = new String(dp.getData());
-                ip=dp.getAddress();
-                String responce=new StringBuilder(info).reverse().toString();;
-                DatagramPacket dps = new DatagramPacket(responce.getBytes(), responce.getBytes().length, ip, dp.getPort());
-                ds.send(dps);
-                handshook=true;
-                System.out.println("server handshook");
-            } catch (Exception e){System.out.println("ERROR");}
-        }
-
-        if (!true)
-        {
-            try{
-                ds = new DatagramSocket();
-                ip = InetAddress.getByName(serverIP);
-                String check="ready";
-                DatagramPacket dp = new DatagramPacket(check.getBytes(), check.getBytes().length,ip,7059);
-                ds.send(dp);
-                byte[] test = new byte[1024];
-                DatagramPacket dpr = new DatagramPacket(test, test.length);
-                ds.receive(dpr);
-                handshook=true;
-            } catch (Exception e){}
-        }
+        try{
+            ds = new DatagramSocket(7059);
+            byte[] test = new byte[1024];
+            DatagramPacket dp = new DatagramPacket(test,test.length);
+            ds.receive(dp);
+            String info = new String(dp.getData());
+            ip=dp.getAddress();
+            String responce=new StringBuilder(info).reverse().toString();;
+            DatagramPacket dps = new DatagramPacket(responce.getBytes(), responce.getBytes().length, ip, dp.getPort());
+            ds.send(dps);
+            handshook=true;
+            System.out.println("server handshook");
+        } catch (Exception e){System.out.println("ERROR");}
     }
 }
